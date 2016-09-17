f311a1a52bca2d3554319458afa2b8ce086ec729
Benjamin Radovsky <radovskyb@gmail.com>
2016-09-17 20:22:09 +1000

    fixed mime package error handling

--- mime/quotedprintable/main.go
+++ mime/quotedprintable/main.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"log"
+	// Import quotedprintable as qp since it's a long name to type.
 	qp "mime/quotedprintable"
 	"os"
 )
@@ -17,7 +19,10 @@ func main() {
 	// Write encodes p using quoted-printable encoding and writes it to the
 	// underlying io.Writer. It limits line length to 76 characters. The encoded
 	// bytes are not necessarily flushed until the Writer is closed.
-	w.Write([]byte("Hello, World!"))
+	_, err := w.Write([]byte("Hello, World!"))
+	if err != nil {
+		log.Fatalln(err)
+	}
 
 	// Binary mode treats the writer's input as pure binary and processes end of
 	// line bytes as binary data.
@@ -25,11 +30,16 @@ func main() {
 
 	// Close closes the Writer, flushing any unwritten data to the underlying
 	// io.Writer, but does not close the underlying io.Writer.
-	w.Close()
+	if err := w.Close(); err != nil {
+		log.Fatalln(err)
+	}
 
 	// NewReader returns a quoted-printable reader, decoding from r.
 	r := qp.NewReader(&buf)
 
 	// Read reads and decodes quoted-printable data from the underlying reader.
-	io.Copy(os.Stdout, r) // Calls the r.Read method underneath
+	_, err = io.Copy(os.Stdout, r) // Calls the r.Read method underneath
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
