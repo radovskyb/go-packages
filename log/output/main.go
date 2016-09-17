5dc4c8f8498d612b466dd8d9c4c811a8630e5475
Benjamin Radovsky <radovskyb@gmail.com>
2016-09-17 20:18:34 +1000

    fixed log package error handling

--- log/output/main.go
+++ log/output/main.go
@@ -12,18 +12,34 @@ func main() {
 	// frames to skip when computing the file name and line number
 	// if Llongfile or Lshortfile is set; a value of 1 will print the details
 	// for the caller of Output.
-	log.Output(0, "log.Output - flag: Llongfile, calldepth: 0\n\n")
-	log.Output(1, "log.Output - flag: Llongfile, calldepth: 1\n\n")
-	log.Output(2, "log.Output - flag: Llongfile, calldepth: 2\n\n")
-	log.Output(3, "log.Output - flag: Llongfile, calldepth: 3\n\n")
+	if err := log.Output(0, "log.Output - flag: Llongfile, calldepth: 0\n\n"); err != nil {
+		log.Fatalln(err)
+	}
+	if err := log.Output(1, "log.Output - flag: Llongfile, calldepth: 1\n\n"); err != nil {
+		log.Fatalln(err)
+	}
+	if err := log.Output(2, "log.Output - flag: Llongfile, calldepth: 2\n\n"); err != nil {
+		log.Fatalln(err)
+	}
+	if err := log.Output(3, "log.Output - flag: Llongfile, calldepth: 3\n\n"); err != nil {
+		log.Fatalln(err)
+	}
 
 	log.SetFlags(log.Lshortfile)
 
 	// We can make calldepth as deep as needed if log.Output is wrapped
 	// in x amount of function calls etc. The greater the abstraction before
 	// calling log.Output, the greater the call depth can be
-	log.Output(0, "log.Output - flag: Lshortfile, calldepth: 0\n\n")
-	log.Output(1, "log.Output - flag: Lshortfile, calldepth: 1\n\n")
-	log.Output(2, "log.Output - flag: Lshortfile, calldepth: 2\n\n")
-	log.Output(3, "log.Output - flag: Lshortfile, calldepth: 3")
+	if err := log.Output(0, "log.Output - flag: Lshortfile, calldepth: 0\n\n"); err != nil {
+		log.Fatalln(err)
+	}
+	if err := log.Output(1, "log.Output - flag: Lshortfile, calldepth: 1\n\n"); err != nil {
+		log.Fatalln(err)
+	}
+	if err := log.Output(2, "log.Output - flag: Lshortfile, calldepth: 2\n\n"); err != nil {
+		log.Fatalln(err)
+	}
+	if err := log.Output(3, "log.Output - flag: Lshortfile, calldepth: 3"); err != nil {
+		log.Fatalln(err)
+	}
 }
